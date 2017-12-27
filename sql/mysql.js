0df2dea2813514a32fb8db37e1b918f699cdf940
yubing <29988076@qq.com>
2017-12-27 16:58:11 +0800

    ..

--- sql/mysql.js
+++ sql/mysql.js
@@ -7,21 +7,45 @@ const pool = mysql.createPool({
     database: 'nodesql'
 });
 
-let query = function(sql,values){
-    return new Promise((resolve,reject)=>{
-         pool.getConnection(function(err,connection){
-             if(err){
-                resolve(err);
-             }else{
-                connection.query(sql,values,(err,rows)=>{
+const query = function(sql,values){
+    return new Promise(function(reslove, reject){
+        pool.getConnection((err,connection)=>{
+            if(err){
+                reject(err);
+            }else{
+                connection.query(sql,values,(err,results)=>{
                     if(err){
                         reject(err);
                     }else{
-                        resolve(rows);
+                        resolve(results);
                     }
-                    connection.release();
                 })
-             }
-         })
+            }
+            connection.release()
+        })
     })
 }
+
+
+
+query().then((value)=>{
+    console.log(value);
+},(err)=>{
+    console.log(err);
+})
+
+
+const createTbale = (sql)=>{
+    query(sql,[]);
+}
+
+//建表
+const users = `create table if not exists users(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    password VARCHAR(40) NOT NULL,
+    PRIMARY KEY (id)
+);`
+
+
+createTbale(users);
